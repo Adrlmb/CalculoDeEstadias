ba077b580058c75f99b02afae2cf897c8c6ddbd2
Adriel Lima <adriellima.barros@gmail.com>
2024-09-12 14:05:44 -0300

    testes de simplificação

--- extrairTexto.py
+++ extrairTexto.py
@@ -3,8 +3,7 @@ pdf = pdfplumber.open('estadia\TICKET 1.pdf')
 page = pdf.pages[0]
 text = page.extract_text()
 # dataExtraida = text.split('\n')[4].split(' ')[2].replace('.', '/')
-hora = text.split('\n')[4].split(' ')[3].split(':')[0]
-minutos = text.split('\n')[4].split(' ')[3].split(':')[1]
+hora = text.split('\n')[10].split('-')[1]
 
 
-print(hora +':'+ minutos)
+print(hora)
