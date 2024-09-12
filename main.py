41f511cfe7752faadab669b866a867ba57120a97
Adriel Lima <adriellima.barros@gmail.com>
2024-09-12 14:05:28 -0300

    organização e refatoração do código

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ from openpyxl import load_workbook
 pdf = pdfplumber.open('estadia\TICKET 1.pdf')
 page = pdf.pages[0]
 text = page.extract_text()
-# print(text.split('\n')[7].split(':')[1])
 
-# Carrega o arquivo existente
-wb = load_workbook('estadia\Cálculo estadia.xlsx')
-
-# Seleciona a planilha ativa
-planilha = wb.active
+wb = load_workbook('estadia\Cálculo estadia.xlsx')  # Carrega o arquivo existente
+planilha = wb.active  # Seleciona a planilha ativa
 
 
 def dataDeSaida():
@@ -21,24 +17,22 @@ def dataDeSaida():
     return data + ' ' + hora + ':' + minutos
 
 
-planilha['C3'] = 'INPUT'  # Fornecedor
+planilha['F3'] = text.split('\n')[10].split('-')[1]  # Transportador
 planilha['C4'] = int(text.split('\n')[7].split(':')[1])  # NF
 planilha['C5'] = text.split('\n')[9].split('-')[1]  # Produto, impor condição se for Rocha!!
-planilha['B9'] = 'INPUT'  # Data e Hora de chegada no formato DD/MM/AA HH:MM
+planilha['F12'] = text.split('\n')[5].split(": ")[1]  # Peso NF com vírgula
 planilha['B15'] = dataDeSaida()  # Data e Hora da saída no formato DD/MM/AA HH:MM
-planilha['F3'] = 'G10'  # Transportador
+
+planilha['B9'] = 'INPUT'  # Data e Hora de chegada no formato DD/MM/AA HH:MM
+planilha['C3'] = 'INPUT'  # Fornecedor
 planilha['F4'] = 'INPUT'  # Ct-e
 planilha['F5'] = 'INPUT'  # Motorista
-planilha['F12'] = text.split('\n')[5].split(": ")[1]  # Peso NF com vírgula
 planilha['E16'] = 'INPUT'  # Motivo, vai se iniciar com "motivo:" e concatenar com o real motivo da estadia
 
-# Lê o valor da célula A1
-valor_celula = planilha['C4'].value
+valor_celula = planilha['C4'].value  # Lê o valor da célula A1
+print(valor_celula)  # Imprime o valor na tela
 
-# Imprime o valor na tela
-print(valor_celula)
-
-wb.save('EstadiasCalculadas\Estadia_0.002.xlsx')  # Salva a planilha com o nome Estadia + o nome do motorista
+wb.save('EstadiasCalculadas\Estadia - ' + planilha['F5'] + '.xlsx')  # Salva a planilha com o nome Estadia + o nome do motorista
 
 #####################################################################################################
 
