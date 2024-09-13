6bf7c0d4efa17ab82fec523a14437cdcfe253d52
Adriel Lima <adriellima.barros@gmail.com>
2024-09-13 09:28:02 -0300

    organização e redução de containers

--- display.py
+++ display.py
@@ -1,11 +1,72 @@
 from tkinter import *
-
 import pdfplumber
 from openpyxl.reader.excel import load_workbook
 
 
 class Application:
     def __init__(self, master=None):
+
+        wb = load_workbook('estadia\Cálculo estadia.xlsx')  # Carrega o arquivo existente
+        planilha = wb.active  # Seleciona a planilha ativa
+
+        pdf = pdfplumber.open('estadia\TICKET 1.pdf')
+        page = pdf.pages[0]
+        text = page.extract_text()
+
+        nomeTransportadora = StringVar()
+        numeroNF = IntVar()
+        nomeProduto = StringVar()
+        pesoNF = StringVar()
+        dataHoraSaida = StringVar()
+        dataHoraChegada = StringVar()
+        nomeFornecedor = StringVar()
+        numeroCte = IntVar()
+        nomeMotorista = StringVar()
+        motivoEstadia = StringVar()
+
+        def dataDeSaida():
+            data = text.split('\n')[4].split(' ')[2].replace('.', '/')
+            hora = text.split('\n')[4].split(' ')[3].split(':')[0]
+            minutos = text.split('\n')[4].split(' ')[3].split(':')[1]
+
+            return data + ' ' + hora + ':' + minutos
+
+        def msg():
+            nomeTransportadora.set(text.split('\n')[10].split('-')[1])
+            numeroNF.set(int(text.split('\n')[7].split(':')[1]))
+            nomeProduto.set(text.split('\n')[9].split('-')[1])
+            pesoNF.set(text.split('\n')[5].split(": ")[1])
+            dataHoraSaida.set(dataDeSaida())
+
+        def preencherPlanilha():
+
+            planilha['F3'] = str(self.inputTransportadora.get())  # Transportador
+            planilha['C4'] = str(self.inputNF.get())  # NF
+            planilha['C5'] = str(self.inputProduto.get())  # Produto, impor condição se for Rocha!!
+            planilha['F12'] = str(self.inputPeso.get())  # Peso NF com vírgula
+            planilha['B15'] = str(self.inputDataHoraSaida.get())  # Data e Hora da saída no formato DD/MM/AA HH:MM
+            planilha['B9'] = str(self.inputDataHoraChegada.get())  # Data e Hora de chegada no formato DD/MM/AA HH:MM
+            planilha['C3'] = str(self.inputFornecedor.get())  # Fornecedor
+            planilha['F4'] = str(self.inputCte.get())  # Ct-e
+            planilha['F5'] = str(self.inputMotorista.get())  # Motorista
+            planilha['E16'] = 'Motivo : ' + str(
+                self.inputMotivo.get())  # Motivo, vai se iniciar com "motivo:" e concatenar com o real motivo da estadia
+
+            nome = planilha['F5'].value
+
+            if nome in ' ':
+                self.inputMotorista['bg'] = 'pink'
+                self.inputMotorista['text'] = 'Preencha todos os campos'
+            else:
+                self.inputMotorista['bg'] = 'white'
+
+            valor_celula = planilha['C3'].value  # Lê o valor da célula C3
+            print(valor_celula)  # Imprime o valor na tela
+
+            wb.save('EstadiasCalculadas\Estadia - ' + nome + '.xlsx')  # Salva a planilha com o nome Estadia + o nome do motorista
+
+        ###########################################################################################################
+
         self.fontePadrao = ("Arial", "10")
 
         self.container1 = Frame(master)
@@ -42,80 +103,6 @@ class Application:
         self.container7["pady"] = 5
         self.container7.pack()
 
-        self.container8 = Frame(master)
-        self.container8["padx"] = 20
-        self.container8["pady"] = 5
-        self.container8.pack()
-
-        self.container9 = Frame(master)
-        self.container9["padx"] = 20
-        self.container9["pady"] = 5
-        self.container9.pack()
-
-        self.container10 = Frame(master)
-        self.container10["padx"] = 20
-        self.container10["pady"] = 15
-        self.container10.pack()
-
-        wb = load_workbook('estadia\Cálculo estadia.xlsx')  # Carrega o arquivo existente
-        planilha = wb.active  # Seleciona a planilha ativa
-
-        pdf = pdfplumber.open('estadia\TICKET 1.pdf')
-        page = pdf.pages[0]
-        text = page.extract_text()
-
-        nomeTransportadora = StringVar()
-        numeroNF = IntVar()
-        nomeProduto = StringVar()
-        pesoNF = StringVar()
-        dataHoraSaida = StringVar()
-        dataHoraChegada = StringVar()
-        nomeFornecedor = StringVar()
-        numeroCte = IntVar()
-        nomeMotorista = StringVar()
-        motivoEstadia = StringVar()
-
-
-        def dataDeSaida():
-            data = text.split('\n')[4].split(' ')[2].replace('.', '/')
-            hora = text.split('\n')[4].split(' ')[3].split(':')[0]
-            minutos = text.split('\n')[4].split(' ')[3].split(':')[1]
-
-            return data + ' ' + hora + ':' + minutos
-
-        def msg():
-            nomeTransportadora.set(text.split('\n')[10].split('-')[1])
-            numeroNF.set(int(text.split('\n')[7].split(':')[1]))
-            nomeProduto.set(text.split('\n')[9].split('-')[1])
-            pesoNF.set(text.split('\n')[5].split(": ")[1])
-            dataHoraSaida.set(dataDeSaida())
-
-        def preencherPlanilha():
-            
-            planilha['F3'] = str(self.inputTransportadora.get())  # Transportador
-            planilha['C4'] = str(self.inputNF.get())  # NF
-            planilha['C5'] = str(self.inputProduto.get())  # Produto, impor condição se for Rocha!!
-            planilha['F12'] = str(self.inputPeso.get())  # Peso NF com vírgula
-            planilha['B15'] = str(self.inputDataHoraSaida.get())  # Data e Hora da saída no formato DD/MM/AA HH:MM
-            planilha['B9'] = str(self.inputDataHoraChegada.get())  # Data e Hora de chegada no formato DD/MM/AA HH:MM
-            planilha['C3'] = str(self.inputFornecedor.get())  # Fornecedor
-            planilha['F4'] = str(self.inputCte.get())  # Ct-e
-            planilha['F5'] = str(self.inputMotorista.get())  # Motorista
-            planilha['E16'] = 'Motivo : ' + str(self.inputMotivo.get())  # Motivo, vai se iniciar com "motivo:" e concatenar com o real motivo da estadia
-
-            nome = planilha['F5'].value
-
-            if nome in ' ':
-                self.inputMotorista['bg'] = 'pink'
-                self.inputMotorista['text'] = 'Preencha todos os campos'
-            else:
-                self.inputMotorista['bg'] = 'white'
-
-            valor_celula = planilha['C3'].value  # Lê o valor da célula C3
-            print(valor_celula)  # Imprime o valor na tela
-
-            wb.save('EstadiasCalculadas\Estadia - ' + nome + '.xlsx')  # Salva a planilha com o nome Estadia + o nome do motorista
-
         # Titulo
         self.title = Label(self.container1, text="Calculo de Estadia")
         self.title["font"] = ("Calibri", "20", "bold")
@@ -133,7 +120,7 @@ class Application:
         # Transportadora
         self.transportadora = Label(self.container2, text="Transportadora ", font=self.fontePadrao)
         self.transportadora.pack(side=LEFT)
-        
+
         self.inputTransportadora = Entry(self.container2, textvariable=nomeTransportadora)
         self.inputTransportadora["width"] = 30
         self.inputTransportadora["font"] = self.fontePadrao
@@ -204,23 +191,25 @@ class Application:
         self.inputPeso.pack(side=LEFT)
 
         # Motivo da Estadia
-        self.motivoEstadia = Label(self.container9, text="Motivo da Estadia ", font=self.fontePadrao)
+        self.motivoEstadia = Label(self.container6, text="Motivo da Estadia ", font=self.fontePadrao)
         self.motivoEstadia.pack(side=LEFT)
 
-        self.inputMotivo = Entry(self.container9)
+        self.inputMotivo = Entry(self.container6)
         self.inputMotivo["width"] = 60
         self.inputMotivo["font"] = self.fontePadrao
         self.inputMotivo.pack(side=LEFT)
 
         # Button - Chama a função que extrai os campos do PDF
-        self.btnBuscar = Button(self.container10, text="Extrair dados do PDF", font=self.fontePadrao, width=20, command=msg)
+        self.btnBuscar = Button(self.container7, text="Extrair dados do PDF", font=self.fontePadrao, width=20,
+                                command=msg)
         self.btnBuscar.pack(side=LEFT)
 
         # Button - Chama a função que salva dados do input
-        self.btnInput = Button(self.container10, text="Emitir Estadia", font=self.fontePadrao, width=20, command=preencherPlanilha)
+        self.btnInput = Button(self.container7, text="Emitir Estadia", font=self.fontePadrao, width=20,
+                               command=preencherPlanilha)
         self.btnInput.pack(side=RIGHT)
+
 
 root = Tk()
 Application(root)
 root.mainloop()
-
