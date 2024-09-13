ba16a43b2ef44a11aacbc112ee8fa36e25f71ffc
Adriel Lima <adriellima.barros@gmail.com>
2024-09-13 09:27:39 -0300

    criada a função para copiar o caminho do arquivo pdf

--- extrairTexto.py
+++ extrairTexto.py
@@ -1,9 +1,21 @@
-import pdfplumber
-pdf = pdfplumber.open('estadia\TICKET 1.pdf')
-page = pdf.pages[0]
-text = page.extract_text()
-# dataExtraida = text.split('\n')[4].split(' ')[2].replace('.', '/')
-hora = text.split('\n')[10].split('-')[1]
+import tkinter as tk
+from tkinter import filedialog
 
 
-print(hora)
+class Tela:
+    def __init__(self, master):
+        self.nossaTela = master
+
+        self.barra_menu = tk.Menu(self.nossaTela)
+        self.nossaTela.config(menu=self.barra_menu)
+
+        self.barra_menu.add_command(label="Escolher PDF", command=self.abreDir)
+
+    def abreDir(self):
+        self.arquivo = filedialog.askopenfile(initialdir = "/Desktop", title = "Selecione um arquivo", filetypes=(("Arquivos PDF", "*.pdf"),("Arquivos de texto", "*.txt")))
+        print(str(self.arquivo).split('\u0027')[1])
+
+
+janelaRaiz = tk.Tk()
+Tela(janelaRaiz)
+janelaRaiz.mainloop()
