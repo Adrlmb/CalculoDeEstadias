e5b7040167c3bc71f78490ba149989438682fc1e
Adriel Lima <adriellima.barros@gmail.com>
2024-09-15 11:31:57 -0300

    teste coverter .py em exe

--- extrairTexto.py
+++ extrairTexto.py
@@ -1,21 +1,4 @@
-import tkinter as tk
-from tkinter import filedialog
+import onefile
+import pyinstaller
 
-
-class Tela:
-    def __init__(self, master):
-        self.nossaTela = master
-
-        self.barra_menu = tk.Menu(self.nossaTela)
-        self.nossaTela.config(menu=self.barra_menu)
-
-        self.barra_menu.add_command(label="Escolher PDF", command=self.abreDir)
-
-    def abreDir(self):
-        self.arquivo = filedialog.askopenfile(initialdir = "/Desktop", title = "Selecione um arquivo", filetypes=(("Arquivos PDF", "*.pdf"),("Arquivos de texto", "*.txt")))
-        print(str(self.arquivo).split('\u0027')[1])
-
-
-janelaRaiz = tk.Tk()
-Tela(janelaRaiz)
-janelaRaiz.mainloop()
+--onefile --noconsole -- display.py
