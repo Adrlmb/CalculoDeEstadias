85b9ac3989ac808458dc45aa0e8f9b6d731e0b61
Adriel Lima <adriellima.barros@gmail.com>
2024-09-20 12:43:23 -0300

    teste de strings

--- tests.py
+++ tests.py
@@ -1,36 +1,17 @@
-from tkinter import *
+transportadoras= ['FUTURO', 'G10', 'CARVALHO', 'FRIBON TRANSPORTES LTDA', 'D GRANEL', 'SIMOES BEBEDOURO', 'AGUETONI']
+transportadoras.sort()
 
-janela = Tk()
-janela.title("Frame")
-janela.geometry()
+nome = 'fri'.upper()
+nome = nome[0:3]
 
 
-label1 = Label(janela, text='Fornecedor')
-label1.grid(row = 0 , column = 0)
 
-input1 = Entry(janela)
-input1.grid(row = 0, column = 1)
-
-blankSpace = Label(janela, width= 10)
-blankSpace.grid(row = 0, column = 2)
-
-label2 = Label(janela, text='Produto')
-label2.grid(row = 0 , column = 3)
-
-test = 'test'
-input2 = Entry(janela, textvariable= test )
-input2.grid(row = 0, column = 4)
-
-label3 = Label(janela, text= 'Data Chegada DD/MM/AAA')
-label3.grid(row = 1, column = 0)
-
-input3 = Entry(janela)
-input3.grid(row = 1, column = 1)
+for i in range(len(transportadoras)):
+    if nome in transportadoras[i]:
+        print(transportadoras[i])
 
 
 
 
 
 
-
-janela.mainloop()
