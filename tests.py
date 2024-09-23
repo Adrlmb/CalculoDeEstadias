6c777d771c665c53b1a743a61afb100c8f8a384a
Adriel Lima <adriellima.barros@gmail.com>
2024-09-23 11:58:55 -0300

    teste de match entre codigo de transportadora e o nome

--- tests.py
+++ tests.py
@@ -1,17 +1,17 @@
-transportadoras= ['FUTURO', 'G10', 'CARVALHO', 'FRIBON TRANSPORTES LTDA', 'D GRANEL', 'SIMOES BEBEDOURO', 'AGUETONI']
-transportadoras.sort()
+import pdfplumber
 
-nome = 'fri'.upper()
-nome = nome[0:3]
+pdf = pdfplumber.open('tickets/belocal - cal.pdf')
+page = pdf.pages[0]
+text = page.extract_text()
 
+transportadoras = ['MINERACAO BELOCAL', 'CARVALHO TRANSPORTES', 'FRIBON TRANSPORTES', 'FUTURO LOGISTICA',
+                   'SIMOES BEBEDOURO', 'TRANSLOPES TRANSPORTES']
 
+codigoTransportadoras = ['2000227172', '2000224719', '2000215499', '2000226886', '204005', '207327']
+
+referencia = text.split('\n')[10].split('-')[0].split(' ')[1]
+print(referencia)
 
 for i in range(len(transportadoras)):
-    if nome in transportadoras[i]:
+    if codigoTransportadoras[i] == referencia:
         print(transportadoras[i])
-
-
-
-
-
-
